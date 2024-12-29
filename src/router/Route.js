b9c8dcd0d0d70a0b04e21e6163f36e55295efc42
Azizkhuja <azizxoja200425@mail.ru>
2024-12-29 15:58:37 +0500

    Initial commit

---
+++ src/router/Route.js
@@ -0,0 +1,9 @@
+export const APP_ROUTES = {
+  URL: 'https://garage-admin.uz/api',
+  LOGIN: '/login',
+  DASHBORAD: '/dashboard',
+  CARS: '/cars',
+  MONITORING: '/monitoring',
+  CHAT: '/chat',
+  MESSAGES: '/messages',
+}
